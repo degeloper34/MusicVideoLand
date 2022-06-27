0db2233224eaf7142713fa2e16359d3ed87a0627
Ege Barış Akyürek <72935668+degeloper34@users.noreply.github.com>
2022-06-28 00:00:14 +0300

    Types for Reducers

--- src/store/reducers/musicVideoReducers.ts
+++ src/store/reducers/musicVideoReducers.ts
@@ -1,11 +1,25 @@
+import {GenreList, MusicVideoList} from "../../../types";
 import {SET_GET_MUSIC_VIDEO_RESPONSE} from "../actions/actionTypes";
 
-const initialState = {
+const initialState: {
+  musicVideoList: MusicVideoList;
+  genreList: GenreList;
+} = {
   musicVideoList: [],
   genreList: [],
 };
 
-function musicVideoReducer(state = initialState, action: any) {
+interface ActionSetGetMusicVideoResponse {
+  type: "SET_GET_MUSIC_VIDEO_RESPONSE";
+  payload: {
+    musicVideoList: MusicVideoList;
+    genreList: GenreList;
+  };
+}
+
+type Actions = ActionSetGetMusicVideoResponse;
+
+function musicVideoReducer(state = initialState, action: Actions) {
   switch (action.type) {
     case SET_GET_MUSIC_VIDEO_RESPONSE:
       return {
--- src/store/reducers/searchReducers.ts
+++ src/store/reducers/searchReducers.ts
@@ -1,10 +1,19 @@
 import {SET_SELECTED_GENRE_ID} from "../actions/actionTypes";
 
-const initialState = {
+const initialState: {
+  selectedGenreId: number;
+} = {
   selectedGenreId: -1,
 };
 
-function musicVideoReducer(state = initialState, action: any) {
+interface ActionSetSelectedGenreId {
+  type: "SET_SELECTED_GENRE_ID";
+  payload: number;
+}
+
+type Actions = ActionSetSelectedGenreId;
+
+function musicVideoReducer(state = initialState, action: Actions) {
   switch (action.type) {
     case SET_SELECTED_GENRE_ID:
       return {...state, selectedGenreId: action.payload};
